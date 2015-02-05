be7acd2231208b258c6856d3495ff8f1bbaa9eac
ABHAY RAIZADA <abhsag24@gmail.com>
2015-02-06 01:30:30 +0530

    Working copy,UI Still buggy, need to look a lot more into the listen function

--- client-server.py
+++ client-server.py
@@ -1,14 +1,36 @@
 import socket
 import func
+from threading import Thread 
 
 choice=raw_input("Connect to a person or start a server 1|2 :\n")
+def sendo(s):
+	data = raw_input("\nYou>>")
+	while True:
+		s.sendall(data)
+		data = raw_input("\nYou>>")
+		if(data == 'q'):
+			s.close()
+			exit()
+			break
+def recvo(s):
+	while True:
+		data=s.recv(1024)
+		if data:
+			print host + ">>"+data
+		if data == 'q':
+			s.close()
+			exit()
+
+			break
+
+
 if choice == '1':
 	s=socket.socket()
 	host = func.return_ip()
 	port = int(raw_input("Enter the port\n"))
 	print ("Server IP | Port are %s | %d" %(host,port))
 	s.bind((host,port))
-	s.listen(5)
+	s.listen(1)
 	print 'Connecting ...'
 	while True:
     	# establish a connection
@@ -16,26 +38,23 @@ if choice == '1':
 		print("Got a connection from %s" % str(addr))
 		try:
 			clientsocket.send('Connection Established')
-			data = raw_input(">>")
-			while True:
-				clientsocket.send(data)
-				data = raw_input(">>")
-				if data == 'q':
-					clientsocket.close()	
-    		    #print s.recv(1024)
+			
+			t1 = Thread(target=sendo,args=(clientsocket,))
+			t2 = Thread(target=recvo, args =(clientsocket,))
+			t1.start()
+			t2.start()
     		
 		except:
-			clientsocket.close()
+			print "FUCK\n"
+			#clientsocket.close()
 if choice == '2':
 	s= socket.socket()
 	host = raw_input("Enter the IP to connect to : ")
 	port = int(raw_input("Enter the port: "))
 	s.connect((host, port))
-
-	data = s.recv(1024)
-	s.sendall(raw_input(""))
-	while True:
-		data = s.recv(1024)
-		if data:
-			print host + ">>"+data;
-	s.close()
+	sendCT = Thread(target=sendo,args=(s,))
+	recvCT = Thread(target=recvo, args =(s,))
+	sendCT.start()
+	recvCT.start()
+	
+	#s.close()
