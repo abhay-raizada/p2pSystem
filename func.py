2d8598563f4b1911e485d11a81cc07d4171f4c0d
Abhay Raizada <abhsag24@gmail.com>
2015-05-24 10:47:05 +0530

    implement cross platforming b/w windows and ubuntu

--- func.py
+++ func.py
@@ -1,8 +1,17 @@
 import subprocess
 import re
+import platform
+
+
 
 #returns ip as string value
-def return_ip():   
-	p=subprocess.check_output("ifconfig")
-	ip = re.search('inet addr:(.+?)  Bcast', p)
-	return ip.group(1)
+def return_ip(): 
+	if platform.system() == "Windows":
+		p=subprocess.check_output("ipconfig")
+		ip = re.search('   IPv4 Address. . . . . . . . . . . : (.+?)\n   Subnet', p)
+		
+		return ip.group(1).strip()
+	else:	  
+		p=subprocess.check_output("ifconfig")
+		ip = re.search('inet addr:(.+?)  Bcast', p)
+		return ip.group(1)
