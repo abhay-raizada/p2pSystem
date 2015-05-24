cfe4fb84ea8340b1ddee4d8c952999b704f044f8
Abhay Raizada <abhsag24@gmail.com>
2015-05-24 10:45:24 +0530

    Update client-server.py

--- client-server.py
+++ client-server.py
@@ -1,93 +1,105 @@
 import socket
 import func
 from threading import Thread 
-import time
-import select
-import re
-name = raw_input("What is your name? \n")
-onlinePeople =[]
-def sendo(s): #SENDING MESSAGE
+
+choice=raw_input("1.HOST A CHAT\n2.JOIN AN EXISTING CHAT:\n3.HOST A GROUP CHAT\n")
+def sendo(s):
 	data = raw_input("")
 	while True:
 		s.sendall(data)
 		data = raw_input("")
 		if(data == 'q'):
-			
+			s.close()
+			exit()
 			break
-	return
-def recvo(s): # RECIEVING MESSAGE
+def recvo(s):
 	while True:
 		data=s.recv(1024)
 		if data:
 			print host + ">>"+data
 		if data == 'q':
-			
+			s.close()
+			exit()
 
 			break
-	return
-
-def broadcast(s,ip): # BROADCASTING NAME AND IP				
-	while 1:
-		data = ip +' '+name
-		s.sendto(data, ('<broadcast>', 6000))
-		time.sleep(2)
-def recvBroadcast(s): # RECIEVING IP
-	
+def sendgrp(L):
+	data = raw_input("")
 	while True:
-    		result = select.select([s],[],[])
-    		msg = result[0][0].recv(1024)
-    		if msg:
-    			ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', msg ) 
-    		if ip[0] not in onlinePeople :
-    			onlinePeople.append(ip[0])
-    			print ip[0] + ' is online\n'
-# CREATING A UDP SOCKET TO SEND
-s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s1.bind(('', 0))
-s1.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-# CREATING A UDP SOCKET TO RECIEVE
-s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s2.bind(('<broadcast>', 6000))
-s2.setblocking(0)
-host = func.return_ip()
-# THREAD TO ALWAYS BROADCAST IP AND ALWAYS SEARCH FOR BROADCASTED IPs
-udpThreadSend= Thread(target=broadcast,args=(s1,host,))	
-udpThreadRecv= Thread(target=recvBroadcast,args=(s2,))	
-#Socket for a tcp connection		
-s=socket.socket()
+		for i in L:
+			i[0].sendall(data)
+		data = raw_input("")
+		
+def recvgrp(L):
+	while True:
+		for i in L:
+			data=i[0].recv(1024)
+			if data:
+				for j in L:
+					j[0].sendall(data)
+				print i[1][0],">>",data
+		
 
-port = 6482
-s.bind((host,port))
-def tcpServer(s):
+
+if choice == '1':
+	s=socket.socket()
+	host = func.return_ip()
+	raw_input("")
+	port = int(raw_input("Enter the port\n"))
+	print ("Server IP | Port are %s | %d" %(host,port))
+	s.bind((host,port))
 	s.listen(1)
 	print 'Listening ...'
 	while True:
-        	 # establish a connection
+    	# establish a connection
 		clientsocket,addr = s.accept()      
 		print("Got a connection from %s" % str(addr))
 		try:
 			clientsocket.send('Connection Established')
-				
-			SendS = Thread(target=sendo,args=(clientsocket,))
-			RecS  = Thread(target=recvo, args =(clientsocket,))
-			SendS.start()
-			RecS.start()
-			clientsocket.close()
-			s.close()
+			
+			t1 = Thread(target=sendo,args=(clientsocket,))
+			t2 = Thread(target=recvo, args =(clientsocket,))
+			t1.start()
+			t2.start()
     		
 		except:
 			print "FUCK\n"
 			#clientsocket.close()
-def tcpClient(s):
+if choice == '2':
 	s= socket.socket()
 	host = raw_input("Enter the IP to connect to : ")
-	port = 6482
+	port = int(raw_input("Enter the port: "))
 	s.connect((host, port))
 	sendCT = Thread(target=sendo,args=(s,))
 	recvCT = Thread(target=recvo, args =(s,))
 	sendCT.start()
 	recvCT.start()
-	s.close()
+	
+if choice =='3':
+	s=socket.socket()
+	L=[]
+	host = func.return_ip()
+	port = int(raw_input("Enter the port\n"))
+	print ("Server IP | Port are %s | %d" %(host,port))
+	s.bind((host,port))
+	s.listen(5)
+	print 'Listening ...'
+	while True:
+    	# establish a connection
+		clientsocket,addr = s.accept()
+		if addr not in L:
+			L.append([clientsocket,addr]) 
+		     
+		print("Got a connection from %s" % str(addr))
+		try:
+			clientsocket.send('Connection Established')
+			
+			tt1 = Thread(target=sendgrp,args=(L,))
+			tt2 = Thread(target=recvgrp, args =(L,))
+			tt1.start()
+			tt2.start()
+    		
+		except:
+			print "FUCK\n"
+			#clientsocket.close()
+	
 	#s.close()
-udpThreadSend.start()
-udpThreadRecv.start()
